e187f7b4723ce63ecfd70b1f3590719a57b81e9d
Your Name <dimasuws2009@gmail.com>
2025-09-23 11:22:20 +0700

    bikin database

---
+++ backend/src/config/db.js
@@ -0,0 +1,19 @@
+import mysql2 from "mysql2/promise"
+
+const pool = mysql2.createPool({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "notes_app"
+})
+
+export const testConnection = async () => {
+    try {
+        const connection = await pool.getConnection()
+        console.log("database berhasil!")
+        connection.release()
+    } catch (error) {
+        console.error("databases gagal di simpan")
+        throw error
+    }
+}
