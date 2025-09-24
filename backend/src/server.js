0cc08763e73197c13fa6678f730bf4dabf2854dc
Your Name <dimasuws2009@gmail.com>
2025-09-24 20:55:08 +0700

    selesai

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,13 +4,14 @@ import helloRouter from "./routes/helloRoute.js"
 import noteRoute from "./routes/notesRoute.js"
 
 const app = express()
-const port = 3000 
+app.use( express.json())
 
+const port = 3000 
 //cara menggunakan router
 app.use( helloRouter)
 app.use( noteRoute)
 
-app.use( express.json())
+
 
 app.listen(port, () => {
     console.log(`server running at http://localhost:${port}`)
