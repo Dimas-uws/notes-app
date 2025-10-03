34943f8e67d3fd421818fe0246641e91cca6de59
Your Name <dimasuws2009@gmail.com>
2025-10-03 11:17:05 +0700

    done yes

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,19 +1,20 @@
-import express from "express"
-import { testConnection } from "./config/db.js"
-import helloRouter from "./routes/helloRoute.js"
-import noteRoute from "./routes/notesRoute.js"
+import express from "express";
+import { testConnection } from "./config/db.js";
+import helloRouter from "./routes/helloRoute.js";
+import noteRoute from "./routes/notesRoute.js";
+import cors from "cors";
 
-const app = express()
-app.use( express.json())
+const app = express();
+app.use(cors());
 
-const port = 3000 
+app.use(express.json());
+
+const port = 3000;
 //cara menggunakan router
-app.use( helloRouter)
-app.use( noteRoute)
-
-
+app.use(helloRouter);
+app.use(noteRoute);
 
 app.listen(port, () => {
-    console.log(`server running at http://localhost:${port}`)
-    testConnection()
-})
+  console.log(`server running at http://localhost:${port}`);
+  testConnection();
+});
